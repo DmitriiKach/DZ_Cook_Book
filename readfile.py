f5a0a3c6fc05854a3e23d5c2eaf587351db13b3e
Dmit.nuclear <dmit.kach@yandex.ru>
2025-01-28 19:01:50 +0500

    fix_errors

--- readfile.py
+++ readfile.py
@@ -99,69 +99,43 @@ def get_shop_list_by_dishes(dishes, person_count):
 '''
 
 
-def rewrite_file(path1=None, path2=None, path3=None):
-    if path1 or path2 or path3 is None:
-        path1 = '1.txt'
-        path2 = '2.txt'
-        path3 = '3.txt'
-        os.chdir('sorted')
-        outout_file = "rewrite_file.txt"
-        file1_path = os.path.join(os.getcwd(), path1)
-        file2_path = os.path.join(os.getcwd(), path2)
-        file3_path = os.path.join(os.getcwd(), path3)
-        with open(file1_path, 'r', encoding='utf-8') as f1:
-            file1 = f1.readlines()
-        with open(file2_path, 'r', encoding='utf-8') as f2:
-            file2 = f2.readlines()
-        with open(file3_path, 'r', encoding='utf-8') as f3:
-            file3 = f3.readlines()
-        with open(outout_file, 'w', encoding='utf-8') as f_total:
+def rewrite_files(directory=None):
+    if directory is None:
+        directory = 'sorted'
 
-            if len(file1) < len(file2) and len(file1) < len(file3):
-                f_total.write(path1 + '\n')
-                f_total.write(str(len(file1)) + '\n')
-                f_total.writelines(file1)
-                f_total.write('\n')
-            elif len(file2) < len(file1) and len(file2) < len(file3):
-                f_total.write(path2 + '\n')
-                f_total.write(str(len(file2)) + '\n')
-                f_total.writelines(file2)
-                f_total.write('\n')
-            elif len(file3) < len(file1) and len(file3) < len(file2):
-                f_total.write(path3 + '\n')
-                f_total.write(str(len(file3)) + '\n')
-                f_total.writelines(file3)
-                f_total.write('\n')
-            if len(file2) > len(file1) > len(file3) or len(file2) < len(file1) < len(
-                    file3):
-                f_total.write(path1 + '\n')
-                f_total.write(str(len(file1)) + '\n')
-                f_total.writelines(file1)
-                f_total.write('\n')
-            elif len(file1) > len(file2) > len(file3) or len(file2) > len(file1) and len(file2) < len(file3):
-                f_total.write(path2 + '\n')
-                f_total.write(str(len(file2)) + '\n')
-                f_total.writelines(file2)
-                f_total.write('\n')
-            elif len(file1) > len(file3) > len(file2) or len(file3) > len(file1) and len(file3) < len(file2):
-                f_total.write(path3 + '\n')
-                f_total.write(str(len(file3)) + '\n')
-                f_total.writelines(file3)
-                f_total.write('\n')
-            if len(file1) > len(file2) and len(file1) > len(file3):
-                f_total.write(path1 + '\n')
-                f_total.write(str(len(file1)) + '\n')
-                f_total.writelines(file1)
-            elif len(file2) > len(file1) and len(file2) > len(file3):
-                f_total.write(path2 + '\n')
-                f_total.write(str(len(file2)) + '\n')
-                f_total.writelines(file2)
-            elif len(file3) > len(file1) and len(file3) > len(file2):
-                f_total.write(path3 + '\n')
-                f_total.write(str(len(file3)) + '\n')
-                f_total.writelines(file3)
-    else:
-        print('Давай лучше без параметров')
+    files_info = {}
+    output_file = "rewrite_file.txt"
+
+    # Формируем путь до директории
+    dir_path = os.path.abspath(directory)
+
+    # Получаем список всех файлов в директории
+    file_paths = [os.path.join(dir_path, filename) for filename in os.listdir(dir_path)]
+
+    # Читаем содержимое каждого файла и сохраняем информацию в словарь
+    for file_path in file_paths:
+        try:
+            with open(file_path, 'r', encoding='utf-8') as f:
+                content = f.readlines()
+                files_info[file_path] = {
+                    'filename': os.path.basename(file_path),
+                    'line_count': len(content),
+                    'content': content
+                }
+        except IOError as e:
+            print(f"Не удалось прочитать файл {file_path}: {e}")
+
+    # Сортируем файлы по количеству строк
+    sorted_files = sorted(files_info.items(), key=lambda x: x[1]['line_count'])
+
+    # Записываем результаты в выходной файл
+    with open(output_file, 'w', encoding='utf-8') as f_total:
+        for file_path, info in sorted_files:
+            f_total.write(info['filename'] + '\n')
+            f_total.write(str(info['line_count']) + '\n')
+            f_total.writelines(info['content'])
+            f_total.write('\n')
+
     return
 
 
@@ -176,4 +150,4 @@ if __name__ == '__main__':
 
     time.sleep(2)
     print('Задание 3------------------------------------------------------------')
-    rewrite_file()
+    rewrite_files()
